ae0e4edc0a8c7561d3acfa37eec0424ff7569075
FarhanRangrez11 <farhanrangrez11@gmail.com>
2025-05-06 12:00:00 +0530

    COMMIT TEST FOR PUSH AND PULL REQUEST

--- src/utils/config.js
+++ src/utils/config.js
@@ -11,7 +11,3 @@ const BASE_URL= "https://construction-backend-production-45b0.up.railway.app/api
 // const BASE_URL = "https://hrb5wx2v-8000.inc1.devtunnels.ms/api";  //sagar sir
 
 export const apiUrl = BASE_URL;
-
-
-
- 
